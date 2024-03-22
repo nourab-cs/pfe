7c123d8f070d10396d35a26d32dc3e6d055a7692
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-03-22 23:12:05 +0100

    pagination implemented

--- client/src/components/layouts/Pagination.jsx
+++ client/src/components/layouts/Pagination.jsx
@@ -12,10 +12,24 @@ function Pagination({ currentPage, totalPages, onPageChange }) {
       onPageChange(currentPage + 1);
     }
   };
-
+  const getPageNumberClass = (pageNumber) => {
+    return pageNumber === currentPage ? "bg-blue-500 text-white" : "text-gray-500";
+  };
   return (
     <div className="flex justify-between mt-4">
       <button onClick={handlePrevPage} disabled={currentPage === 1} className="text-blue-600 disabled:text-gray-400">Previous</button>
+
+      <div>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index}
+            onClick={() => onPageChange(index + 1)}
+            className={`px-2 py-1 mx-1 rounded-full focus:outline-none focus:ring bg-transparent ${getPageNumberClass(index + 1)}`}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
       <button onClick={handleNextPage} disabled={currentPage === totalPages} className="text-blue-600 disabled:text-gray-400">Next</button>
     </div>
   );
