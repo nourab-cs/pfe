dad5d38144d1c91af55bc141a1b802ad8d01ad57
nourab-cs <aboudanour123@gmail.com>
2024-03-28 15:33:19 +0100

    password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -159,9 +159,10 @@ console.log(req);
 }
 
 const forgotPassword  = async (req, res) => {
-  const {email} = req.body;
-  UserModel.findOne({email: email})
-  .then(user => {
+  {/*const {email} = req.body;
+  
+ const user=await UserModel.findOne({email: email})
+  console.log(user);
       if(!user) {
           return res.send({Status: "User not existed"})
       } 
@@ -187,28 +188,28 @@ const forgotPassword  = async (req, res) => {
           } else {
             return res.send({Status: "Success"})
           }
-        });
-  })
+        });*/}
+
+  
 };
 
-
 const resetPassword = async(req, res) => {
-  const {id, token} = req.params
-  const {password} = req.body
+  // const {id, token} = req.params
+  // const {password} = req.body
 
-  jwt.verify(token, "jwt_secret_key", (err, decoded) => {
-      if(err) {
-          return res.json({Status: "Error with token"})
-      } else {
-          bcrypt.hash(password, 10)
-          .then(hash => {
-              UserModel.findByIdAndUpdate({_id: id}, {password: hash})
-              .then(u => res.send({Status: "Success"}))
-              .catch(err => res.send({Status: err}))
-          })
-          .catch(err => res.send({Status: err}))
-      }
-  })
+  // jwt.verify(token, "jwt_secret_key", (err, decoded) => {
+  //     if(err) {
+  //         return res.json({Status: "Error with token"})
+  //     } else {
+  //         bcrypt.hash(password, 10)
+  //         .then(hash => {
+  //             UserModel.findByIdAndUpdate({_id: id}, {password: hash})
+  //             .then(u => res.send({Status: "Success"}))
+  //             .catch(err => res.send({Status: err}))
+  //         })
+  //         .catch(err => res.send({Status: err}))
+  //     }
+  // })
 };
 
 module.exports = {
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -13,8 +13,8 @@ router.post("/register", require("../controllers/auth.controller").register);
 router.post("/login", require("../controllers/auth.controller").login);
 router.get("/logout", require("../controllers/auth.controller").logout);
 router.get("/oauth", require("../controllers/auth.controller").oauth);
-router.post("/forgot-password",require(("../controllers/auth.controller").forgotPassword));
-router.post("/reset-password",require(("../controllers/auth.controller").resetPassword));
+//router.post("/forgot-password",require(("../controllers/auth.controller").forgotPassword));
+//router.post("/reset-password/:id/:token",require(("../controllers/auth.controller").resetPassword));
 
 
 module.exports = router;
