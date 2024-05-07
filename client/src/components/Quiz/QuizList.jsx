49ed498553e0fddc25c23364b998fce2a2d701f6
dalyyy3259@protonmail.com <dalyyy3259@protonmail.com>
2024-05-07 17:23:30 +0100

    create quiz and display quiz

--- client/src/components/Quiz/QuizList.jsx
+++ client/src/components/Quiz/QuizList.jsx
@@ -1,20 +1,41 @@
 import { useEffect, useState } from "react";
 import { axiosClient } from "../../services/axiosClient";
+import QuizForm from "./CreateQuiz";
 
 function QuizList() {
   const [quizes, setQuizes] = useState([]);
+  const [show, setShow] = useState(false);
+
   useEffect(() => {
     axiosClient
       .get("/quiz/all")
       .then((res) => {
-        console.log(res.data);
-        setQuizes(res.data);
+        setQuizes(res?.data);
       })
       .then((e) => {
         console.log(e);
       });
   }, []);
-  return <div>QuizList</div>;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        Add new Quiz
+      </button>
+      {show ? (
+        <QuizForm />
+      ) : (
+        <div>
+          {quizes.map((e, i) => {
+            return <div key={i}>{e.name}</div>;
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default QuizList;
